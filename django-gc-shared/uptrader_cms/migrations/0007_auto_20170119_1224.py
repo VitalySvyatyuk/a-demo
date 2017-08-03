5186aa9b41bc1982d03769e4d18cf8cb7cb114ea
Vitaly Svyatyuk <goodgame1945@gmail.com>
2017-08-03 16:13:20 +0300

    agents and representatives to Partners

--- django-gc-shared/uptrader_cms/migrations/0007_auto_20170119_1224.py
+++ django-gc-shared/uptrader_cms/migrations/0007_auto_20170119_1224.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='legaldocument',
             name='category',
-            field=models.CharField(max_length=100, verbose_name='Category', choices=[(b'docs', 'Documents'), (b'policy', 'Privacy Policy'), (b'risks', 'Risk Disclosure'), (b'regulation', 'Regulation'), (b'cookies', 'Cookie Policy'), (b'agents', 'Agents and Representatives')]),
+            field=models.CharField(max_length=100, verbose_name='Category', choices=[(b'docs', 'Documents'), (b'policy', 'Privacy Policy'), (b'risks', 'Risk Disclosure'), (b'regulation', 'Regulation'), (b'cookies', 'Cookie Policy'), (b'agents', 'Partners and Representatives')]),
         ),
     ]
